64a2f581436af0cd21383d1628ad65640eac81d1
Pritam Patidar <168543405+prtmptdr@users.noreply.github.com>
2024-06-08 10:27:42 +0530

    Add files via upload

---
+++ stl.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<array>
+#include<vector>
+using namespace std;
+int main(){
+// array<int,4>arr={10 ,20,30,50};
+// cout<<arr.at(3)<<endl;
+// cout<<arr.front()<<endl;
+// cout<<arr.back()<<endl;
+vector<int>vec={40,50,20};
+//cout<<vec.at(2);
+vec.push_back(78);
+cout<<vec.at(3);
+}
